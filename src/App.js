d04c611c4678d357399e7ec820db897ca7c9cd16
Yashodha <hettiarachchi.yashodha@gmail.com>
2020-10-20 23:22:23 +0530

    basic-model

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './home.component.js';
+import HomePage from './components/home-page/home.component.jsx';
 
 function App() {
   return (
---
+++ src/components/home-page/home.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './home.styles.scss';
+import Directory from '../directory/directory.component';
+
+const HomePage = ()=>(
+  <div className='home-page'>
+    <Directory/>
+  </div>
+)
+
+export default HomePage;
