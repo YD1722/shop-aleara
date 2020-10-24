462227caddc6a9ab03b31de70a2e4e55a24ffe7a
Yashodha <hettiarachchi.yashodha@gmail.com>
2020-10-24 22:14:01 +0530

    add preview for shop items

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React from 'react';
 import './App.css';
 import HomePage from './components/home-page/home.component.jsx';
 import {Switch,Route} from 'react-router-dom';
-
-
-const HatsPage = ()=>(
-  <h1>Hats Page</h1>
-)
- 
+import ShopPage from './components/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/hats' component={HatsPage}/>
+        <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
